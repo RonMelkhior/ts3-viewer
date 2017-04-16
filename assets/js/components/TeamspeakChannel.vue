ecb4b163799b852488e72365c81a005e40bed655
Ron Melkhior <ron3353@gmail.com>
2017-04-16 09:29:17 +0300

    Website: Added icons to clients+channels.

--- assets/js/components/TeamspeakChannel.vue
+++ assets/js/components/TeamspeakChannel.vue
@@ -2,6 +2,9 @@
     <div class="item">
         <div class="level">
             <div class="level-left">
+                <span class="level-item">
+                    <img src="/img/channel.svg" v-if="!channel.channel.spacer">
+                </span>
                 <span class="level-item" v-text="channel.channel.name"></span>
             </div>
 
@@ -16,7 +19,22 @@
                     <div class="item" v-for="client in channel.clients">
                         <div class="level">
                             <div class="level-left">
-                                <span class="level-item" v-text="client.name"></span>
+                                <span class="level-item">
+                                    <img src="/img/away.svg" v-if="client.away">
+                                    <img src="/img/sound-off.svg" v-else-if="client.soundMuted || client.soundDisabled">
+                                    <img src="/img/mic-off.svg" v-else-if="client.micMuted || client.micDisabled">
+                                    <img src="/img/person.svg" v-else>
+                                </span>
+                                <span class="level-item">
+                                    {{ client.name }}
+                                </span>
+                            </div>
+
+                            <div class="level-right">
+                                <span class="level-item">
+                                    <img src="/img/mic.svg" v-if="client.talkPower < channel.channel.neededTalkPower && client.isTalker">
+                                    <img src="/img/mic-off.svg" v-if="client.talkPower < channel.channel.neededTalkPower && !client.isTalker">
+                                </span>
                             </div>
                         </div>
                     </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ mix
 .js('assets/js/vendor.js', 'public/js')
 .js('assets/js/app.js', 'public/js')
 .sass('assets/sass/vendor.sass', 'public/css')
-.sass('assets/sass/app.sass', 'public/css');
+.sass('assets/sass/app.sass', 'public/css')
+.copyDirectory('assets/img', 'public/img');
